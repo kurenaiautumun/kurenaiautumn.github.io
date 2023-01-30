7f0d0d8538f825ceab9a374644e295e3479132ff
swayammaheshwari <swayammaheshwari890@gmail.com>
2023-01-30 13:44:11 +0530

    localStrategy debug

--- passportConfig.js
+++ passportConfig.js
@@ -30,9 +30,21 @@ module.exports = (passport) => {
     module.exports = (passport) => {
           passport.use(
             "local-signup",
-            new LocalStrategy(
-         // sign up
-            )
+            new LocalStrategy( {
+                    usernameField: "email",
+                    passwordField: "password",
+                  },
+                  async (email, password, done) => {
+                    try {
+                      const user = await emailExists(email);
+                      if (!user) return done(null, false);
+                      const isMatch = await matchPassword(password, user.password);
+                      if (!isMatch) return done(null, false);
+                      return done(null, {id: user.id, email: user.email});
+                    } catch (error) {
+                      return done(error, false);
+                    }
+                  })
           );
           passport.use(
             "local-login",
