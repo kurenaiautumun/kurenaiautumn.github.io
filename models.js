18b0083d1a5c12056d433df9d697e22468eed9bc
swayammaheshwari <swayammaheshwari890@gmail.com>
2023-03-13 23:54:33 +0530

    recommandation updated

--- models.js
+++ models.js
@@ -9,7 +9,8 @@ const  userSchema = new mongoose.Schema({
     email:String,
     password: String,
     followers:Array,
-    following:Array
+    following:Array,
+    recommandation:Array
   });
 userSchema.plugin(passportLocalMongoose, {usernameQueryFields: ["username","email"]});
 
--- routes/recomandation.js
+++ routes/recomandation.js
@@ -1,15 +1,57 @@
-const express=require('express');
-const { User } = require('../models');
-const router=express.Router()
+const express = require("express");
+const { User, Blog } = require("../models");
+const router = express.Router();
 
-router.get('/recomandation/:userId',(req,res)=>{
-    
-    User.find().exec((err,post)=>{
-        if (err) throw err;
-        console.log(post)
-    })
-})
+router.post("/recommandation/:userId", async (req, res) => {
+  const _id = req.params.userId;
+  const { recommandation } = req.body;
+  User.updateOne({ _id }, { $push: { recommandation } }, function (err, docs) {
+    if (!err) {
+      res.status(201).json({ message: "update succesfully", docs });
+    }
+  });
+});
+
+router.get("/recommandation/:userId", async (req, res) => {
+    let _id = req.params.userId;
+    let {recommandation} = await User.findOne({_id});
+    var allBlog={};
+    recommandation.forEach(async element => {
+        let blog = await Blog.find({
+            $or: [{ title: { $regex: element, $options: "i" } }],
+        }).exec();
+        allBlog = { ...allBlog, ...blog };
+        console.log(allBlog)
+    });
+    res.status(201).send({allBlog})
+});
+
+// router.get("/recommandation/:userId", async (req, res) => {
+//     let _id = req.params.userId;
+//     try {
+//         const user = await User.findOne({ _id });
+//         let { recommandation } = user;
+//         let allBlog = {};
+//         for (const element of recommandation) {
+//             const posts = await Blog.find({
+//                 $or: [{ title: { $regex: element, $options: "i" } }],
+//             }).exec();
+//             allBlog = { ...allBlog, ...posts };
+//         }
+//         const uniqueBlogs = Object.values(
+//             allBlog.reduce((acc, cur) => {
+//                 if (!acc[cur._id]) {
+//                     acc[cur._id] = cur;
+//                 }
+//                 return acc;
+//             }, {})
+//         );
+//         console.log(uniqueBlogs);
+//     } catch (err) {
+//         console.error(err);
+//     }
+// });
 
 
 
-module.exports=router;
+module.exports = router;
